a680363b346d6c0aec4721f796ff34b7ddd524f2
Yashi <55877273+Yashi-Kesarwani@users.noreply.github.com>
2020-03-20 12:29:44 +0530

    Update 003-6kyu-Find-the-odd-int.py

--- 003-6kyu-Find-the-odd-int.py
+++ 003-6kyu-Find-the-odd-int.py
@@ -1,10 +1,8 @@
 # https://www.codewars.com/kata/54da5a58ea159efa38000836
-import collections
+
 
 def find_it(seq):
-  if len( seq ) == 0:
+    for i in seq:
+        if seq.count(i)%2!=0:
+            return i
     return None
-  b = collections.Counter(seq)
-  for c in b:
-    if b[c] % 2 ==1:
-      return c
